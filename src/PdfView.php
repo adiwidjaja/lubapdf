aeeb360dd06b97068a6c060049d149cefb1da627
Andy Adiwidjaja <adiwidjaja@gmail.com>
2016-10-01 23:54:56 +0200

    Snappy options.

--- src/PdfView.php
+++ src/PdfView.php
@@ -7,6 +7,24 @@ use Knp\Snappy\Pdf;
 
 class PdfView extends View
 {
+    /**
+     * Variables passed to snappy
+     */
+    protected $options = [];
+
+    public function __construct($template, array $variables = [], $customPath = NULL, $compileVars = false)
+    {
+        parent::__construct($template, $variables, $customPath, $compileVars);
+    }
+
+    public function setOption($name, $value) {
+        $this->options[$name] = $value;
+    }
+
+    public function setOptions($options) {
+        $this->options = $options;
+    }
+
     public function output($filename="file.pdf", $download=false)
     {
         $html = $this->render();
@@ -23,12 +41,12 @@ class PdfView extends View
             header('Content-Disposition: attachment; filename="'.$filename.'"');
         else
             header('Content-Disposition: inline; filename="'.$filename.'"');
-        echo $snappy->getOutputFromHtml($html);
+        echo $snappy->getOutputFromHtml($html, $this->options);
     }
 
     public function saveToFile($filename)
     {
         $html = $this->render();
-        $snappy->generateFromHtml($html, $filename);
+        $snappy->generateFromHtml($html, $filename, $this->options);
     }
 }
